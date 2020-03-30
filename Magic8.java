501cbbeb778d13d5fa1fe9bc59c80508e5ac3437
Krunal Chauhan <59173615+Whokrunalchauhan@users.noreply.github.com>
2020-03-30 12:56:34 +0530

    update
    rather than using switch case i decided to use ArrayList.

--- Magic8.java
+++ Magic8.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 
 
 public class Magic8{
@@ -8,38 +8,24 @@ public class Magic8{
 
         //this part will genrate Random number
         Random random = new Random();
-        int num = random.nextInt(12)+1;
-        //this is switch case to give ans
-        switch(num)
-        {
-            case 1: System.out.println("It is certain");
-                break;
-            case 2: System.out.println("Reply hazy, try again");
-                break;
-            case 3: System.out.println("Don’t count on it.");
-                break;
-            case 4: System.out.println("It is decidedly so.");
-                break;
-            case 5: System.out.println("Ask again later.");
-                break;
-            case 6: System.out.println("My reply is no");
-                break;
-            case 7: System.out.println("Without a doubt.");
-                break;
-            case 8: System.out.println("Better not tell you now.");
-                break;
-            case 9: System.out.println("My sources say no.");
-                break;
-            case 10: System.out.println("Yes – definitely");
-                break;
-            case 11: System.out.println("Cannot predict now");
-                break;
-            case 12: System.out.println("Outlook not so good.");
-                break;
-            default : System.out.println("hmm... this must be an error then.");
-
-        }
-
+        int num = random.nextInt(12);
+        
+        ArrayList<String> answer = new ArrayList<String>();
+        answer.add("It is certain");
+        answer.add("Reply hazy, try again");
+        answer.add("Don’t count on it.");
+        answer.add("It is decidedly so.");
+        answer.add("Ask again later.");
+        answer.add("My reply is no");
+        answer.add("Without a doubt.");
+        answer.add("Better not tell you now.");
+        answer.add("My sources say no.");
+        answer.add("Yes – definitely");
+        answer.add("Cannot predict now");
+        answer.add("Outlook not so good.");
+      
+      System.out.println(answer.get(num));
+      
 
 
     }
